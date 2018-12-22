810b2e04741a1d95e3b04d182f04504398150dbc
jasonlin <jasonlin@elevenbuckets.com>
2018-12-21 22:34:55 -0700

    adding half done contract

--- contracts/Gameths.sol
+++ contracts/Gameths.sol
@@ -4,10 +4,10 @@ import "./ERC20.sol";
 
 contract Battleship {
 	// Variables
-	address public playerA;
+	address public defender;
 	address public winner;
 	uint constant public maxPlayer = 1000;
-	uint constant public period = 5;
+	uint constant public period = 10;
 	uint public initHeight;
 	bytes32 public difficulty;
 	uint public fee = 10000000000000000;
@@ -18,16 +18,28 @@ contract Battleship {
 		address wallet; // msg.sender
 		uint since;     // block height when joined
 		bytes32 ticket; // board
-		uint8[64] slots;
+		bool[32] slots;
 		uint8 v;
 		bytes32 r;
 		bytes32 s; 
 	}
 
+	struct battleStat {
+		uint battle;
+		bytes32 merkleRoot;
+		bytes32 secret;
+		bytes32 score;
+	}
+
 	mapping (address => playerInfo) playerDB;
-	mapping (uint => address) playerList;
+	mapping (uint => mapping (address => battleStat)) battleHistory;
 
 	// Modifiers
+	modifier defenderOnly() {
+		require(msg.sender == defender);
+		_;
+	}
+
 	modifier WinnerOnly() {
 		require(winner != address(0) && msg.sender == winner);
 		_;
@@ -48,32 +60,27 @@ contract Battleship {
 		_;
 	}
 
-	modifier notYourself() {
-		require(msg.sender != playerA);
+	modifier notDefender() {
+		require(msg.sender != defender);
 		_;
 	}
 
 	// Contract constructor
-	constructor(bytes32 _difficulty) payable feePaid {
-		playerA = msg.sender;
-		initHeight = block.number;
+	constructor(bytes32 _difficulty, bytes32 _init) payable feePaid {
+		defender = msg.sender;
 		difficulty = _difficulty;
 
-		// PlayerA board
-		playerInfo memory newone;
-
-		newone.wallet = msg.sender;
-		newone.since  = block.number;
-		newone.ticket = sha3(sha3(abi.encodePacked(msg.sender, blockhash(block.number - 1))));
-
-		playerDB[msg.sender] = newone;
-		playerList[playercount] = msg.sender; 
-		playercount += 1;
+		fortify(_init);
 	}
 
 	// WinnerOnly
 	function withdraw() WinnerOnly returns (bool) {
+		require(block.number > initHeight + period);
 		require(msg.sender.send(this.balance));
+		setup = false;
+		winner = address(0);
+		playercount = 0;
+
 		return true;
 	}
 
@@ -86,9 +93,23 @@ contract Battleship {
 		return (playerDB[msg.sender].since, playerDB[msg.sender].ticket);
 	}
 
-	// Join game (registration)
-	function register(uint8 v, bytes32 r, bytes32 s, uint[64] slots) payable feePaid notYourself returns (bool) {
-		require(playerDB[msg.sender].since == 0);
+	function fortify(bytes32 defense) defenderOnly NewGameOnly returns (bool) {
+		playerInfo memory newone;
+
+		newone.wallet = msg.sender;
+		newone.since  = block.number;
+		newone.ticket = defense;
+
+		initHeight = block.number;
+		playerDB[msg.sender] = newone;
+		playercount += 1;
+
+		return true;
+	}
+
+	// Join game
+	function challenge(uint8 v, bytes32 r, bytes32 s, bool[32] slots) payable feePaid notDefender returns (bool) {
+		require(playerDB[msg.sender].since < initHeight);
 		require(playercount + 1 <= maxPlayer);
 
 		playerInfo memory newone;
@@ -96,15 +117,39 @@ contract Battleship {
 		newone.wallet = msg.sender;
 		newone.since  = block.number;
 		newone.v = v; newone.r = r; newone.s = s;
+		newone.slots = slots;
 
 		playerDB[msg.sender] = newone;
-		playerList[playercount] = msg.sender; 
 		playercount += 1;
 
 		return true;
 	}
 
-	function revealSecret(bytes32 secret) returns (bool) {
+	function revealSecret(bytes32 secret) notDefender returns (bool) {
+		require(block.number <= initHeight + period && block.number >= initHeight + 5);
+		require(playerDB[msg.sender].since > initHeight);
+		require(ecrecover(sha3("\x19Ethereum Signed Message:\n32", sha256(secret)), playerDB[msg.sender].v, playerDB[msg.sender].r, playerDB[msg.sender].s) == msg.sender);
+
+		battleStat memory newbat;
+		newbat.battle = initHeight;
+		newbat.secret = secret;
+		newbat.score  = keccak256(abi.encodePacked(secret, blockhash(initHeight))); // initialize for the loop below
+
+		for (uint i = 0; i <= 31; i++) {
+			if(playerDB[msg.sender].slots[i] == false) {
+				newbat.score[i] = playerDB[defender].ticket[i];
+			}
+		}
+
+		battleHistory[initHeight][msg.sender] = newbat;
+
+		if (Winner == address(0) && newbat.score < playerDB[defender].ticket) {
+			Winner = msg.sender;
+		} else if (newbat.score < battleHistory[initHeight][Winner].score) {
+			Winner = msg.sender;
+		}
+
+		return true;
 	}
 
 	// fallback

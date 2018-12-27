7979e540bdea55718979d1f421c8ef1955981b5c
jasonlin <jasonlin@elevenbuckets.com>
2018-12-27 13:13:08 -0700

    first draft of off-chain code. still need random secret bank + scoring strategies

---
+++ dapps/BattleShip/BattleShip.js
@@ -0,0 +1,79 @@
+'use strict';
+
+const fs   = require('fs');
+const path = require('path');
+const ethUtils = require('ethereumjs-utils');
+
+// 11BE BladeIron Client API
+const BladeIronClient = require('bladeiron_api');
+
+class BattleShip extends BladeIronClient {
+	constructor(rpcport, rpchost, options)
+        {
+		super(rpcport, rpchost, options);
+		this.ctrName = 'BattleShip';
+		this.secretBank = ['The','Times','03','Jan','2009','Chancellor','on','brink','of','second','bailout','for','banks'];
+		this.target = '0x0000000000000000ffffffffffffffffffffffffffffffffffffffffffffffff';
+		this.address = this.configs.account;
+		this.gameStarted = false;
+		this.initHeight  = 0;
+		this.results = {};
+
+		this.probe = () => 
+		{
+			this.call(this.ctrName)('board')().then((rc) => { this.board = rc; console.log(`The Board: ${rc}`); });
+			this.call(this.ctrName)('setup')().then((rc) => { this.gameStarted = rc; console.log(`The game started = ${rc}`); });
+			this.call(this.ctrName)('initHeight')().then((rc) => { 
+				this.initHeight = rc; console.log(`Init Block Height = ${rc}`); 
+				if (typeof(this.results[this.initHeight]) === 'undefined') this.results[this.initHeight] = [];
+			});
+
+			return Promise.resolve(this.gameStarted);
+		}	
+	
+		this.trial = (stats) => 
+		{
+			if (!this.gameStarted) return [];
+			if (stats.blockHeight < this.initHeight + 5) return [];
+
+			let blockNo = stats.blockHeight;
+			this.secretBank.map((s) => 
+			{
+				let secret = ethUtils.sha256(s);
+				let [myboard, slots] = this.call(this.ctrName)('testOutcome')(secret, blockNo);
+				let score = [ ...myboard ];
+
+				for (i = 0; i <= 31; i ++) {
+					if (!slots[i]) {
+						score[2+i*2] = board.charAt(2+i*2);
+						score[2+i*2+1] = board.charAt(2+i*2);
+					}
+				}
+
+				this.results[this.initHeight].push({score: score.join(''), secret, blockNo, slots});
+			})
+		}
+
+		this.stopTrial = () => 
+		{
+			this.client.unsubscribe('ethstats');
+			this.client.off('ethstats');
+		}
+
+		this.startTrial = () => 
+		{
+			this.probe().then((started) => 
+			{
+				if(started) {
+					this.client.subscribe('ethstats');
+					this.client.on('ethstats', this.trial);
+					console.log('Game started !!!');
+				} else {
+					console.log('Game is not yet set ...');
+				}
+			})
+		}
+	}
+}
+
+module.exports = BattleShip;
---
+++ dapps/BattleShip/Conditions/BattleShip/BattleShip/Sanity.js
@@ -0,0 +1,70 @@
+module.exports = 
+{
+	BattleShip_withdraw_sanity(addr, jobObj) 
+	{
+		let initHeight = this.CUE[jobObj.type][jobObj.contract].initHeight();
+		let period = this.CUE[jobObj.type][jobObj.contract].period();
+
+		if ( addr === this.CUE[jobObj.type][jobObj.contract].winner() 
+		  && this.web3.eth.blockNumber > initHeight + period
+		){
+			return true;
+		} else {
+			return false;
+		}
+	},
+
+	BattleShip_fortify_sanity(addr, jobObj)
+	{
+		if ( addr === this.CUE[jobObj.type][jobObj.contract].defender()
+		  && jobObj.txObj.value >= this.CUE[jobObj.type][job.contract].fee()
+		  && this.CUE[jobObj.type][job.contract].setup === false
+		) {
+			return true;
+		} else {
+			return false;
+		}
+	},
+
+	BattleShip_challenge_sanity(addr, jobObj)
+	{
+		let initHeight = this.CUE[jobObj.type][jobObj.contract].initHeight();
+		let since = this.CUE[jobObj.type][jobOBj.contract].myInfo()[0];
+
+		if ( addr !== this.CUE[jobObj.type][jobObj.contract].defender()
+		  && jobObj.txObj.value >= this.CUE[jobObj.type][job.contract].fee()
+		  && this.CUE[jobObj.type][job.contract].setup === true
+		  && since < initHeight;
+		) {
+			return true;
+		} else {
+			return false;
+		}
+	},
+
+	BattleShip_revealSecret_sanity(addr, jobObj)
+	{
+		let initHeight = this.CUE[jobObj.type][jobObj.contract].initHeight();
+		let period = this.CUE[jobObj.type][jobObj.contract].period();
+		let since = this.CUE[jobObj.type][jobOBj.contract].myInfo()[0];
+		let blockNumber = this.web3.eth.blockNumber;
+
+		// revealSecret(secret, score, slots, blockNo)
+		let [ secret, score, slots, blockNo ] = jobObj.args.map((i) => { return jobObj[i] });
+
+		if ( addr !== this.CUE[jobObj.type][jobObj.contract].defender()
+		  && this.CUE[jobObj.type][job.contract].setup === true
+		  && since > initHeight
+		  && blockNumber <= initHeight + period
+		  && blockNumber >= since + 5
+		  && blockNumber - blockNo < period - 5
+		  && blockNo <= blockNumber - 1
+	 	  && blockNo < initHeight + period
+		  && blockNo >= initHeight + 5  // Note that here we skip signature checking
+		) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}

af97af6562c15be0e0758766e9ceacb73963e8d6
jasonlin <jasonlin@elevenbuckets.com>
2018-12-27 13:12:35 -0700

    first draft of off-chain code. still need random secret bank + scoring strategies

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,6 @@
-var Gameths = artifacts.require("./Gameths.sol");
-var Randoms = artifacts.require("./random.sol");
+var BattleShip = artifacts.require("./BattleShip.sol");
+var ERC20 = artifacts.require("./ERC20.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(Gameths);
-  deployer.deploy(Randoms);
+  deployer.deploy(BattleShip, '0xa82e7cfb30f103af78a1ad41f28bdb986073ff45b80db71f6f632271add7a32e', {value: '10000000000000000'});
 };

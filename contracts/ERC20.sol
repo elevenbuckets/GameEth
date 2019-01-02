ca3cbd61cffd1eac11dfce2174c684f1ccdb3fe7
jasonlin <jasonlin@elevenbuckets.com>
2019-01-02 14:37:46 -0700

    adding RNT token and preparing for meta mining

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -6,10 +6,10 @@ pragma solidity ^0.4.24;
  * @dev see https://github.com/ethereum/EIPs/issues/179
  */
 contract ERC20Basic {
-  uint256 public totalSupply;
-  function balanceOf(address who) public view returns (uint);
-  function transfer(address to, uint value) public returns (bool);
-  event Transfer(address indexed from, address indexed to, uint value);
+  function totalSupply() public view returns (uint256);
+  function balanceOf(address who) public view returns (uint256);
+  function transfer(address to, uint256 value) public returns (bool);
+  event Transfer(address indexed from, address indexed to, uint256 value);
 }
 
 /**
@@ -17,8 +17,8 @@ contract ERC20Basic {
  * @dev see https://github.com/ethereum/EIPs/issues/20
  */
 contract ERC20 is ERC20Basic {
-  function allowance(address owner, address spender) public view returns (uint);
-  function transferFrom(address from, address to, uint value) public returns (bool);
-  function approve(address spender, uint value) public returns (bool);
-  event Approval(address indexed owner, address indexed spender, uint value);
+  function allowance(address owner, address spender) public view returns (uint256);
+  function transferFrom(address from, address to, uint256 value) public returns (bool);
+  function approve(address spender, uint256 value) public returns (bool);
+  event Approval(address indexed owner, address indexed spender, uint256 value);
 }
---
+++ contracts/RNT.sol
@@ -0,0 +1,61 @@
+pragma solidity ^0.4.24;
+import "./StandardToken.sol";
+
+contract RNT is StandardToken {
+    string private _symbol = "RNT";
+    uint8 private _decimals = 12;
+    uint256 public _maxSupply = 100000000000000000000000;
+    address public owner;
+    address public mining;
+    uint256 public reward = 150000000000000;
+
+    modifier ownerOnly() {
+       require(msg.sender == owner);
+       _;
+    }
+
+    modifier miningOnly() {
+       require(msg.sender == mining);
+       _;
+    }
+
+    constructor() public {
+       owner = msg.sender;
+       _totalSupply = 300000000000000000;
+       _balances[0xB440ea2780614b3c6a00e512f432785E7dfAFA3E] = 100000000000000000;
+       _balances[0x4AD56641C569C91C64C28a904cda50AE5326Da41] = 100000000000000000;
+       _balances[0x362ea687b8a372a0235466a097e578d55491d37f] = 100000000000000000;
+    }
+
+    function symbol() public view returns (string){
+        return _symbol;
+    }
+
+    function decimals() public view returns (uint8){
+        return _decimals;
+    }
+
+    // TODO: Making mining contract upgradable while limit owner from changing this arbitrarily
+    function setMining(address miningAddress) ownerOnly public external returns (bool) {
+	require(mining == address(0)); // For now, only allow this to be called once.
+	mining = miningAddress;
+
+	return true;
+    }
+
+    function mint(address toAddress) miningOnly public external returns (bool) {
+	require(_totalSupply < _maxSupply);
+	_balances[toAddress] += reward;
+	_totalSupply += reward;
+
+	return true;
+    }
+
+    function burn(uint256 amount) public external returns (bool) {
+	require(_balances[msg.sender] >= amount);
+	_balances[msg.sender] -= amount;
+	_totalSupply -= amount;
+
+	return true;
+    }
+}

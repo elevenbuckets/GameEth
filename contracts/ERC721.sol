c7c2dee8d7916ae57f2915119b7de7afb0ef7bd1
KaiLung Sun <kaisun@elevenbuckets.com>
2019-02-07 15:34:02 -0700

    add ERC721 contracts

---
+++ contracts/ERC721.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.5.2;
+// ERC721 adapted from https://github.com/0xcert/ethereum-erc721
+import "./ERC721/tokens/nf-token-enumerable.sol";
+import "./ERC721/tokens/nf-token-metadata.sol";
+import "./ERC721/ownership/ownable.sol";
+
+contract Erebor is NFTokenEnumerable, NFTokenMetadata, Ownable {
+    constructor() public {
+        nftName = "Erebor's treasure";
+        nftSymbol = "ERE";
+    }
+
+    function mint(address _to, uint256 _tokenId, string calldata _uri) external onlyOwner {
+        super._mint(_to, _tokenId);
+        super._setTokenUri(_tokenId, _uri);
+    }
+
+    function burn(uint256 _tokenId) external onlyOwner{
+        super._burn(_tokenId);
+    }
+
+    function transferOwnership(address _newOwner) public onlyOwner{
+        super.transferOwnership(_newOwner);
+    }
+}

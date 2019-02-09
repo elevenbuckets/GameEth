b0bda14843db2a55311dba1eefa6128ccba11328
KaiLung Sun <kaisun@elevenbuckets.com>
2019-02-09 15:55:04 -0700

    add ELEMinterface

---
+++ contracts/ELEMInterface.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.5.2;
+import './erc721_interface.sol';
+// import './erc721-enumerable.sol';
+
+contract iELEM is ERC721 {
+    function mint(address _to, uint256 _tokenId, string calldata _uri) external returns (bool);
+    function burn(uint256 _tokenId) external returns (bool);
+}

c93c7f625e77492c67b0d9c52900320bef453ccf
KaiLung Sun <kaisun@elevenbuckets.com>
2019-02-12 20:52:23 -0700

    update membership contract

--- contracts/membership.sol
+++ contracts/membership.sol
@@ -5,22 +5,29 @@ import "./ELEMInterface.sol";
 // import "./erc721_interface.sol";
 // import "./erc721-enumerable.sol";
 
-contract membership {
+
+contract MemberShip {
     address public owner;
     address[3] public managers;
     address public ELEMAddr;
     uint public fee = 10000000000000000;
     uint public memberPeriod = 20000; // 20000 blocks ~ a week, for test only
     bytes32 public difficulty = 0x0000ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff;
-    struct memberInfo{
+    bool public paused = false;
+
+    struct MemberInfo {
         address addr;
         uint since;  // beginning blockNo of previous membership
-        uint penalty;  // the membership is valid until: since + memberPeriod - penalty
+        // addPenalty() make sure penalty is always less than block.number
+        uint penalty;  // the membership is valid until: since + memberPeriod - penalty;
         bytes32 kycid;  // know your customer id, leave it for future
         string notes;
     }
-    mapping (uint => memberInfo) memberDB;  // NFT to memberInfo
-    mapping (address => uint) addressToId;  // address to membership (is this essential?)
+
+    mapping (uint => MemberInfo) internal memberDB;  // NFT to MemberInfo
+    mapping (address => uint) internal addressToId;  // address to membership (is this essential?)
+
+    mapping (address => bool) public appWhitelist; //
 
     constructor(address _ELEMAddr) public {
         owner = msg.sender;
@@ -32,10 +39,20 @@ contract membership {
         // allocate membership here?
     }
 
-    modifier managerOnly(uint _tokenId)  {
+    modifier ownerOnly() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    modifier coreManagerOnly() {
+        require(msg.sender == managers[0] || msg.sender == managers[1] || msg.sender == managers[2]);
+        _;
+    }
+
+    modifier managerOnly(uint _tokenId) {
         require(msg.sender == managers[0] || msg.sender == managers[1] || msg.sender == managers[2]
-	        || _tokenId % 7719472615821079694904732333912527190217998977709370935963838933860875309329 == 0);
-	// uint256(0x1111111111111111111111111111111111111111111111111111111111111111 = 7.71947....e72
+                || _tokenId % 7719472615821079694904732333912527190217998977709370935963838933860875309329 == 0);
+        // uint256(0x1111111111111111111111111111111111111111111111111111111111111111 = 7.71947....e72
         _;
     }
 
@@ -54,7 +71,7 @@ contract membership {
     modifier isActiveMember(uint _tokenId) {
         require(memberDB[_tokenId].addr == msg.sender && msg.sender != address(0));
         require(memberDB[_tokenId].since + memberPeriod - memberDB[_tokenId].penalty > block.number);
-        // is it possible that penalty >= since?
+        // is it possible that penalty >= since or block.number?
         _;
     }
 
@@ -73,10 +90,20 @@ contract membership {
     // }
 
     modifier isNotSpecialToken(uint _tokenId) {
-	// require(_tokenId > uint256(0x0000ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff));
-	// require(_tokenId % uint256(0x1111111111111111111111111111111111111111111111111111111111111111 != 0));
-	require(_tokenId > 1766847064778384329583297500742918515827483896875618958121606201292619775);
-	require(_tokenId % 7719472615821079694904732333912527190217998977709370935963838933860875309329 != 0);
+        // require(_tokenId > uint256(0x0000ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff));
+        // require(_tokenId % uint256(0x1111111111111111111111111111111111111111111111111111111111111111 != 0));
+        require(_tokenId > 1766847064778384329583297500742918515827483896875618958121606201292619775);
+        require(_tokenId % 7719472615821079694904732333912527190217998977709370935963838933860875309329 != 0);
+        _;
+    }
+
+    modifier whenNotPaused() {
+        require(!paused);
+        _;
+    }
+
+    modifier whenPaused {
+        require(paused);
         _;
     }
 
@@ -85,31 +112,31 @@ contract membership {
     // // The first tokens are mined and sold by manager with relatively low price
     // }
 
-    function bindMembership(uint _tokenId) public isNFTOwner(_tokenId) isNotSpecialToken(_tokenId) returns (bool){
+    function bindMembership(uint _tokenId) public isNFTOwner(_tokenId) isNotSpecialToken(_tokenId) whenNotPaused returns (bool) {
         require(memberDB[_tokenId].since == 0 && memberDB[_tokenId].addr == address(0));
-	require(addressToId[msg.sender] == 0);  // the address is not yet bind to any NFT id
+        require(addressToId[msg.sender] == 0);  // the address is not yet bind to any NFT id
         // require ...
-        memberDB[_tokenId] = memberInfo(msg.sender, block.number, 0, bytes32(0), '');
+        memberDB[_tokenId] = MemberInfo(msg.sender, block.number, 0, bytes32(0), "");
         addressToId[msg.sender] = _tokenId;
         return true;
     }
 
-    function unbindMembership(uint _tokenId) public isNFTOwner(_tokenId) isMember(_tokenId) returns (bool){
+    function unbindMembership(uint _tokenId) public isNFTOwner(_tokenId) isMember(_tokenId) whenNotPaused returns (bool) {
         // active member cannot unbind, i.e., cannot trasnfer membership/Token
         require(memberDB[_tokenId].since + memberPeriod - memberDB[_tokenId].penalty < block.number);
-        memberDB[_tokenId] = memberInfo(address(0), 0, 0, bytes32(0), '');
+        memberDB[_tokenId] = MemberInfo(address(0), 0, 0, bytes32(0), "");
         addressToId[msg.sender] = 0;
         return true;
     }
 
-    function renewMembership(uint _tokenId) public payable isMember(_tokenId) isNFTOwner(_tokenId) returns (uint){
+    function renewMembership(uint _tokenId) public payable isMember(_tokenId) isNFTOwner(_tokenId) whenNotPaused returns (uint) {
         require(block.number > memberDB[_tokenId].since + memberPeriod - 10000);  // arbitrary, ~3.5 days before expiration
         // require ...
         memberDB[_tokenId].since = block.number;
         return block.number;
     }
 
-    function assginKYCid(uint _tokenId, bytes32 _kycid) external managerOnly(_tokenId) returns (bool){
+    function assginKYCid(uint _tokenId, bytes32 _kycid) external managerOnly(_tokenId) returns (bool) {
         // instead of "managerOnly", probably add another group to do that
         require(memberDB[_tokenId].since > 0 && memberDB[_tokenId].addr != address(0));
         // require ...
@@ -117,16 +144,99 @@ contract membership {
         return true;
     }
 
-    // function membershipGiveaway(address _addr, uint _tokenId) public managerOnly(_tokenId) isNFTOwner(_tokenId) returns (bool){
-    //     // assume the token is valid and one of the managers owns the token
+    function addWhitelistApps(address _addr) public coreManagerOnly returns (bool) {
+        appWhitelist[_addr] = true;
+        return true;
+    }
+
+    function addPenalty(uint _tokenId, uint _penalty) external returns (uint) {
+        require(appWhitelist[msg.sender] == true);
+        require(memberDB[_tokenId].since > 0);  // is a member
+        require(_penalty < memberPeriod);  // prevent too much penalty
+
+        // extreme case which is unlike to happen
+        if (memberDB[_tokenId].penalty + _penalty > block.number) {
+            memberDB[_tokenId].penalty = block.number - 1;  // if 0 then not a member
+        } else {
+            memberDB[_tokenId].penalty += _penalty;
+        }
+        return memberDB[_tokenId].penalty;
+    }
+
+    function readNotes(uint _tokenId) external view returns (string memory) {
+        require(memberDB[_tokenId].since > 0);
+        return memberDB[_tokenId].notes;
+    }
+
+    function addNotes(uint _tokenId, string calldata _notes) external managerOnly(_tokenId) {
+        require(memberDB[_tokenId].since > 0);
+        memberDB[_tokenId].notes = _notes;
+    }
+
+    // function membershipGiveaway(address _addr, uint _tokenId) public coreManagerOnly(_tokenId) isNFTOwner(_tokenId) returns (bool){
+    //     // assume the token is valid and owned by manager
     //     require(memberDB[_tokenId].addr == address(0));  // no one use the token as member
     //     require(addressToId[_addr] == 0);  // the addr is not yet a member
     //     // require ...
-    //     memberDB[_tokenId] = memberInfo(_addr, block.number, 0, bytes32(0), '');
+    //     memberDB[_tokenId] = MemberInfo(_addr, block.number, 0, bytes32(0), '');
     //     addressToId[_addr] = _tokenId;
     //     string memory uri = '';
     //     require(iELEM(ELEMAddr).mint(_addr, _tokenId, uri) == true);
     //     return true;
     // }
+
+    // some query functions
+    function addrIsMember(address _addr) external view returns (bool) {
+        require(_addr != address(0));
+        if (addressToId[_addr] != 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function addrIsActiveMember(address _addr) external view returns (bool) {
+        require(_addr != address(0));
+        uint _tokenId = addressToId[_addr];
+        if (_tokenId != 0 && memberDB[_tokenId].since + memberPeriod - memberDB[_tokenId].penalty > block.number) {
+            return true;
+        }
+        return false;
+    }
+
+    function tokenIsMember(uint _tokenId) external view returns (bool) {
+        if (memberDB[_tokenId].addr != address(0)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function tokenIsActiveMember(uint _tokenId) external view returns (bool) {
+        if (memberDB[_tokenId].since + memberPeriod - memberDB[_tokenId].penalty > block.number) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function addrToTokenId(address _addr) external view returns (uint) {
+        return addressToId[_addr];
+    }
+
+    // upgradable
+    function pause() external coreManagerOnly whenNotPaused {
+        paused = true;
+    }
+
+    function unpause() public ownerOnly whenPaused {
+        // set to ownerOnly in case accounts of other managers are compromised
+        paused = false;
+    }
+
+    function updateELEMAddr(address _addr) external ownerOnly {
+        //require(ELEMAddr == address(0)); // comment to allow this to be changed many times
+        ELEMAddr = _addr;
+    }
 
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,17 +1,19 @@
-var Erebor = artifacts.require("./Erebor.sol");
-var ERC20 = artifacts.require("./ERC20.sol");
-var StandardToken = artifacts.require("./StandardToken.sol");
-var RNT = artifacts.require("./RNT.sol");
-var SafeMath = artifacts.require("./SafeMath.sol");
-var ELEMInterface = artifacts.require("./ELEMInterface.sol");
+var Erebor = artifacts.require("Erebor");
+var ERC20 = artifacts.require("ERC20");
+var StandardToken = artifacts.require("StandardToken");
+var RNT = artifacts.require("RNT");
+var SafeMath = artifacts.require("SafeMath");
+var memberShip = artifacts.require("MemberShip");
+
 
 module.exports = function(deployer) {
     deployer.deploy(SafeMath);
     deployer.link(SafeMath, [StandardToken, RNT, Erebor]);
     deployer.deploy(StandardToken);
+    deployer.link(StandardToken, RNT);
     deployer.deploy(RNT).then(() => {
         let RNTAddr = RNT.address;
-        let ELEMAddr = '';  // need to fill in an address
+        let ELEMAddr = '0x5c0C5B0E0f93D7e15C67E76153111cEAC6d17AAc';
         return deployer.deploy(
             Erebor,
             '0xa82e7cfb30f103af78a1ad41f28bdb986073ff45b80db71f6f632271add7a32e',

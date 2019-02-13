0a206fa45f4b6f59c9a7b7805d94ea097bf26466
jasonlin <jasonlin@elevenbuckets.com>
2019-02-13 15:56:31 -0700

    adding default dummy conditions. update json

---
+++ dapps/Erebor/Conditions/Erebor/MemberShip/Sanity.js
@@ -0,0 +1,27 @@
+module.exports = 
+{
+ 	MemberShip_buyToken_sanity(addr, jobObj)
+        {
+		return true;
+	},
+
+ 	MemberShip_transferToken_sanity(addr, jobObj)
+        {
+		return true;
+	},
+
+ 	MemberShip_bindMembership_sanity(addr, jobObj)
+        {
+		return true;
+	},
+	
+ 	MemberShip_unbindMembership_sanity(addr, jobObj)
+        {
+		return true;
+	},
+	
+ 	MemberShip_renewMembership_sanity(addr, jobObj)
+        {
+		return true;
+	}
+}
